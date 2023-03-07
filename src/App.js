82c4a083a6da715607e00579235fb7a48d3f7fbe
Mark <mgnorrish@gmail.com>
2023-03-07 17:54:53 +1100

    can query the server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import { lazy, Suspense } from 'react';
-
-const EventDetailsPage = lazy(() => import('./EventDetailsPage.jsx'));
+import { lazy } from 'react'; // and Suspense
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+
+const EventDetailsPage = lazy(() => import('./EventDetailsPage.jsx'));
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/new" element={<NewEventPage />}/>
+        {/* <Route path="/new" element={<NewEventPage />}/> */}
         <Route path="/event/:eventId" element={({ match }) => <EventDetailsPage eventId={match.params.eventId} />}/>
         <Route exact path="/" element={<HomePage />}/>
       </Routes>
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,7 +1,22 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 export default function HomePage() {
-  return <div>
-    <p>
-      Home page, ss
-    </p>
-  </div>
+  const [helloData, setHelloData] = useState(null);
+
+  useEffect(() => {
+    async function fetchHelloData() {
+      const url = "https://us-central1-dance-pair-server-42dc0.cloudfunctions.net/helloWorld";
+      const response = await axios.get(url);
+      setHelloData(response.data);
+    }
+
+    fetchHelloData();
+  }, []);
+
+  return (
+    <div>
+      <p>{helloData ? String(helloData) : "Loading..."}</p>
+    </div>
+  );
 }
